9a314310c1c801cea5428c8759fc61c8a5004297
Guillermo Maschwitz <guilledevel@gmail.com>
2019-11-29 12:22:51 -0300

    theme - using Sass css precompiler from now on

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -49,6 +49,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `gatsby-plugin-mdx`
-  ],
+    `gatsby-plugin-mdx`,
+    {
+      resolve: "gatsby-plugin-sass",
+      options: {
+        useResolveUrlLoader: {
+          options: {
+            sourceMap: true
+          }
+        }
+      }
+    }
+  ]
 }

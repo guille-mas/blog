0b72650fbad98977329da4ec752d337763408f64
Guillermo Maschwitz <guilledevel@gmail.com>
2019-11-29 12:25:47 -0300

    theme - open new tab for external links

--- blog/src/components/contact-menu.js
+++ blog/src/components/contact-menu.js
@@ -2,19 +2,28 @@ import React from "react"
 
 const ContactMenu = () => (
   <nav className="contact">
-    <a className="linkedin" href="https://www.linkedin.com/in/guillermomas/">
+    <a
+      target="_blank"
+      className="linkedin"
+      href="https://www.linkedin.com/in/guillermomas/"
+    >
       LinkedIn
     </a>
-    <a className="github" href="https://github.com/guille-mas">
+    <a target="_blank" className="github" href="https://github.com/guille-mas">
       Github
     </a>
     <a
+      target="_blank"
       className="stackoverflow"
       href="https://stackoverflow.com/users/1607360/guillermo-maschwitz"
     >
       StackOverflow
     </a>
-    <a className="dockerhub" href="https://hub.docker.com/u/guillermomaschwitz">
+    <a
+      target="_blank"
+      className="dockerhub"
+      href="https://hub.docker.com/u/guillermomaschwitz"
+    >
       DockerHub
     </a>
   </nav>

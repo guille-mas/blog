b5f9b8c94a8ff29de77e77399b2553d303e72a1e
Guillermo Maschwitz <guilledevel@gmail.com>
2019-11-29 12:30:06 -0300

    contact links - fixed security issue

--- blog/src/components/contact-menu.js
+++ blog/src/components/contact-menu.js
@@ -4,16 +4,23 @@ const ContactMenu = () => (
   <nav className="contact">
     <a
       target="_blank"
+      rel="noopener noreferrer"
       className="linkedin"
       href="https://www.linkedin.com/in/guillermomas/"
     >
       LinkedIn
     </a>
-    <a target="_blank" className="github" href="https://github.com/guille-mas">
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className="github"
+      href="https://github.com/guille-mas"
+    >
       Github
     </a>
     <a
       target="_blank"
+      rel="noopener noreferrer"
       className="stackoverflow"
       href="https://stackoverflow.com/users/1607360/guillermo-maschwitz"
     >
@@ -21,6 +28,7 @@ const ContactMenu = () => (
     </a>
     <a
       target="_blank"
+      rel="noopener noreferrer"
       className="dockerhub"
       href="https://hub.docker.com/u/guillermomaschwitz"
     >

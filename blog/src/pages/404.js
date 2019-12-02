f3bdf73705fe5eef29053cd9d72f5417c17326b8
Guillermo Maschwitz <guilledevel@gmail.com>
2019-12-02 13:31:17 -0300

    theme - improved error page

--- blog/src/pages/404.js
+++ blog/src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Helmet from "react-helmet"
-import "./about-page.scss"
+import "./error-page.scss"
 
 const NotFoundPage = () => (
   <Layout>

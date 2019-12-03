a3542ea011fd866c53de6f70c3bcda309c403994
Guillermo Maschwitz <guilledevel@gmail.com>
2019-12-03 13:14:47 -0300

    theme - basic blog posts markup

--- blog/src/components/blog/post.js
+++ blog/src/components/blog/post.js
@@ -1,8 +1,23 @@
 import React from "react"
-import "./variables.scss"
+import "./post.scss"
+import Avatar from "../avatar"
 
 class Post extends React.Component {
-
+    render() {
+        return (
+            <article className="blog-post">
+                <header>
+                    <h3>{this.props.title}</h3>
+                </header>
+                <aside>
+                    <Avatar /> 
+                    <address>Guillermo Maschwitz</address>
+                    <small>{this.props.date}</small>
+                </aside>
+                {this.props.body}
+            </article>
+        );
+    }
 }
 
 export default Post
--- blog/src/pages/blog.js
+++ blog/src/pages/blog.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Helmet from 'react-helmet'
 import "./blog-page.scss"
+import Post from "../components/blog/post"
 
 const IndexPage = () => (
   <Layout>
@@ -10,6 +11,47 @@ const IndexPage = () => (
     <Helmet>
       <body className="blog" />
     </Helmet>
+
+    <Post 
+      title="7 Habits of highly effective development teams" 
+      date="Dec 2019"
+    />
+
+    <Post
+      title="Clean, portable development environments using Docker and Docker Compose"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="Agile, stable deployments with Git Flow"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="Javascript refactoring tips - RequireJS"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="An introduction to RxJs"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="PHP refactoring tips - Generators"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="Clean code and code docummentation as critical communication channels"
+      date="Dec 2019"
+    />
+
+    <Post
+      title="Optimize your code for humans first"
+      date="Dec 2019"
+    />
+
   </Layout>
 )
 

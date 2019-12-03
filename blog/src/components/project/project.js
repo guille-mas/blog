e59aa9a3833fee9a478a20c53241896c20b12ce7
Guillermo Maschwitz <guilledevel@gmail.com>
2019-12-03 12:28:08 -0300

    theme - moving project items markup to its own React component

--- blog/src/components/project/project.js
+++ blog/src/components/project/project.js
@@ -1,8 +1,25 @@
 import React from "react"
 import "./project.scss"
 
-class Project {
+class Project extends React.Component {
+    render() {
+        const tech_stack = (this.props.tools || []).map( (tool) => {
+            return (
+                <span>{tool}</span>
+            );
+        });
 
+        return (
+            <article className="project">
+                <header>
+                    <div className="date">{this.props.date}</div>
+                    <h3>{this.props.title}</h3>
+                </header>
+                {this.props.body}
+                <aside className="tech-stack">{tech_stack}</aside>
+            </article>
+        )
+    }
 }
 
 export default Project
--- blog/src/pages/projects.js
+++ blog/src/pages/projects.js
@@ -3,37 +3,57 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Helmet from 'react-helmet'
 import "./projects-page.scss"
+import Project from "../components/project/project"
 
 class ProjectsListPage extends React.Component {
   render() {
+    const dummy_text = (
+      <p>
+        Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.<br></br>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    );
+
     return (
       <Layout>
         <SEO title="Projects" />
         <Helmet>
           <body className="projects" />
         </Helmet>
-        <article className="project">
-          <header>
-            <div className="date">12-3-2019</div>
-            <h3>Project Title</h3>
-          </header>
-          <p>
-              Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.<br></br>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <aside className="tech-stack">
-            <span>docker</span>
-            <span>docker-compose</span>
-            <span>php7</span>
-            <span>typescript</span>
-          </aside>
-        </article>
+
+        <Project 
+          date="12-3-2019" 
+          title="This blog" 
+          body={dummy_text} 
+          tools={['docker', 'docker-compose', 'react', 'terraform', 'aws']} 
+        />
+
+        <Project
+          date="10-20-2019"
+          title="Super Dupper Project"
+          body={dummy_text}
+          tools={['php7', 'typescript', 'docker']}
+        />
+
+        <Project
+          date="8-15-2019"
+          title="A Project I will never forget"
+          body={dummy_text}
+          tools={['terraform', 'aws-ecs', 'aws-lambda', 'node']}
+        />
+
+        <Project
+          date="12-3-2019"
+          title="Web Platform refactor"
+          body={dummy_text}
+          tools={['php7', 'javascript', 'requirejs', 'webpack']}
+        />
       </Layout>
     )
   }

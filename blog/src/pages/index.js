f367c2aa5a42800649ed8b1d80411239fb6bf498
Guillermo Maschwitz <guilledevel@gmail.com>
2019-12-03 12:08:32 -0300

    theme - alignment improvements

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -15,22 +15,27 @@ const AboutPage = () => (
         <h3>About Me</h3>
       </header>
       <p>
-      I am a human oriented full stack developer, inspired by devops principles. 
-      <br/>
-      I love to help remote teams and organizations improve their streams of value through toughtful planning,
-      design, and implementation of high quality software, refactor of legacy codebases, and automatization of cloud
-      infrastructure.
-      I love to work with great teams to build products that people love. 
-      <br/>
-      I am skilled on every level of the tech stack of a web application; from infrastructure as code, through backend and frontend.
-      I love to learn about industry best practices, processes, and also from my fellow coworkers and supervisors.
-      <br/>
-      To avoid burn out I like to run by the coast a couple miles, biking, or taking a walk with my wife.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+      ut aliquip ex ea commodo consequat. 
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+      dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p>
-      I am also the proud parent of a wonderful 3 year old kid.
-      <br />
-      We are very excited, expecting another child that should born on March 2020.
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+      doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+      veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam 
+      voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia 
+      consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+      Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, 
+      adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et 
+      dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum 
+      exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
+      consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse 
+      quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas 
+      nulla pariatur?
       </p>
     </article>
   </Layout>

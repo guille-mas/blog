3248eec0fa88a55715e1c14bc92625ce4e52d15e
Guillermo Maschwitz <guilledevel@gmail.com>
2019-12-03 09:54:19 -0300

    theme - badges and shadows mixins and reusable classes

--- blog/src/pages/projects.js
+++ blog/src/pages/projects.js
@@ -4,13 +4,39 @@ import SEO from "../components/seo"
 import Helmet from 'react-helmet'
 import "./projects-page.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <Helmet>
-      <body className="projects" />
-    </Helmet>
-  </Layout>
-)
+class ProjectsListPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Projects" />
+        <Helmet>
+          <body className="projects" />
+        </Helmet>
+        <article className="project">
+          <header>
+            <div className="date">12-3-2019</div>
+            <h3>Project Title</h3>
+          </header>
+          <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.<br></br>
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <aside className="tech-stack">
+            <span>docker</span>
+            <span>docker-compose</span>
+            <span>php7</span>
+            <span>typescript</span>
+          </aside>
+        </article>
+      </Layout>
+    )
+  }
+}
 
-export default IndexPage
+export default ProjectsListPage

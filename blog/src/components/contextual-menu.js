17620e72f9fc8f19d60b0ead475f54b7a676b9b9
Guillermo Maschwitz <guilledevel@gmail.com>
2020-02-19 20:08:26 -0300

    removed in progress sections

--- blog/src/components/contextual-menu.js
+++ blog/src/components/contextual-menu.js
@@ -8,8 +8,6 @@ import "./contextual-menu.scss"
 const ContextualMenu = () => (
   <nav id="contextual-menu">
     <Link to="/">Blog</Link>
-    <Link to="/projects/index.html">Projects</Link>
-    <Link to="/books/index.html">Books</Link>
     <Link to="/about/index.html">About</Link>
     <Link to="/business/index.html">Services</Link>
   </nav>

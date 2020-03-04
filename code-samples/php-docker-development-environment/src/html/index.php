7a2da508ccb31a9c2c243e629c569e7741578184
Guillermo Maschwitz <guilledevel@gmail.com>
2020-03-04 19:50:19 -0300

    tutorial - several fixes to the code sample

---
+++ code-samples/php-docker-development-environment/src/html/index.php
@@ -0,0 +1,6 @@
+<?php
+
+phpinfo();
+
+echo "HELLO ".getenv("HELLO_NAME");
+
--- code-samples/php-docker-development-environment/src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str = "Xdebug";
-function ret_ord($c) {
-    return ord($c);
-}
-
-foreach (str_split($str) as $char) {
-    echo $char, ": ", ret_ord($char), "\n";
-}
